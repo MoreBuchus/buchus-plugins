d3cf4128e152eba635bd64f9bf9d9885b422c5ea
washer <washer8394@gmail.com>
2025-02-03 18:41:01 -0500

    Update DefenceTrackerPlugin.java
    ** Updated deprecated API call **
    - Added missing dependency (ObjectUtils)
    - Removed unused variable shadowBarrage

--- src/main/java/com/partydefencetracker/DefenceTrackerPlugin.java
+++ src/main/java/com/partydefencetracker/DefenceTrackerPlugin.java
@@ -79,6 +79,7 @@ import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
 import net.runelite.client.util.ColorUtil;
 import net.runelite.client.util.Text;
+import org.apache.commons.lang3.ObjectUtils;
 
 @PluginDescriptor(
 	name = "Party Defence Tracker",
@@ -131,7 +132,6 @@ public class DefenceTrackerPlugin extends Plugin
 	private VulnerabilityInfoBox vulnBox = null;
 	private SpritePixels vuln = null;
 	private ShadowBarrageInfoBox shadowBarrageBox = null;
-	private SpritePixels shadowBarrage = null;
 	private boolean shadowBarrageHit = false;
 
 	private RedKerisInfoBox redKerisBox = null;
@@ -218,7 +218,6 @@ public class DefenceTrackerPlugin extends Plugin
 		redKerisBox = null;
 		redKerisTicks = 0;
 		shadowBarrageBox = null;
-		shadowBarrage = null;
 		shadowBarrageHit = false;
 		bloatDown = false;
 		queuedNpc = null;
@@ -306,7 +305,7 @@ public class DefenceTrackerPlugin extends Plugin
 		specialList.clear();
 		if (partyService.isInParty())
 		{
-			for (NPC n : client.getNpcs())
+			for (NPC n : client.getTopLevelWorldView().npcs())
 			{
 				if (n != null && n.getName() != null && (n.getName().equalsIgnoreCase(boss) || (n.getName().contains("Tekton") && boss.equalsIgnoreCase("Tekton")))
 					&& (n.isDead() || n.getHealthRatio() == 0))
