da8bb0a7b7c833e3f20bf45adcd916624b58c812
robuuuste <robuuste@proton.me>
2025-10-09 12:30:21 +0200

    Refactor: rename HighlightColor > HighlightInfo and update usages

---
+++ src/main/java/com/betternpchighlight/HighlightInfo.java
@@ -0,0 +1,44 @@
+package com.betternpchighlight;
+
+import java.awt.Color;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HighlightInfo {
+    public enum TileStyle {
+        REGULAR,
+        DASHED,
+        CORNER
+    }
+
+    boolean isHighlight;
+    Color color;
+    Color fill;
+    boolean raveOutline;
+    boolean raveFill;
+    int raveSpeed;
+    private TileStyle tileStyle;
+    double outlineWidth;
+    boolean antiAliasing;
+    int outlineFeather;
+
+    public HighlightInfo(boolean isHighlight, Color color, Color fill) {
+        this(isHighlight, color, fill, false, false, 6000, TileStyle.REGULAR, 2.0, true, 2); // Default values
+    }
+
+    public HighlightInfo(boolean isHighlight, Color color, Color fill, boolean raveOutline, boolean raveFill, int raveSpeed, TileStyle tileStyle, double outlineWidth, boolean antiAliasing, int outlineFeather) {
+        this.isHighlight = isHighlight;
+        this.color = color;
+        this.fill = fill;
+        this.raveOutline = raveOutline;
+        this.raveFill = raveFill;
+        this.raveSpeed = raveSpeed;
+        this.tileStyle = tileStyle;
+        this.outlineWidth = outlineWidth;
+        this.antiAliasing = antiAliasing;
+        this.outlineFeather = outlineFeather;
+    }
+}
